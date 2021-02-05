b9aace4c3cabe9399dd22868565f041f80f1a591
DufClément <37572780+DufClement@users.noreply.github.com>
2021-02-05 12:41:28 +0100

    Add Docker

---
+++ build-images.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+docker build -t spark-base:latest ./docker/base
+docker build -t spark-master:latest ./docker/spark-master
+docker build -t spark-worker:latest ./docker/spark-worker
+docker build -t spark-submit:latest ./docker/spark-submit
+docker build -t pyspark-notebook:latest ./docker/pyspark_notebook
